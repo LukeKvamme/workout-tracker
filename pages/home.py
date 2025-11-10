3526700697c13d4f22a845f66e6691ae1443d8c4
Kvamme, Luke Brynjulv <lkvamme3@gatech.edu>
2025-11-09 20:15:10 -0500

    Change: Home Page text

--- pages/home.py
+++ pages/home.py
@@ -4,6 +4,6 @@ import dash
 dash.register_page(__name__, path='/')
 
 layout = html.Div([
-    html.H1('This is our Home page'),
-    html.Div('This is our Home page content.'),
+    html.H1('IDK What else to put here'),
+    html.Div('But I am changing things so i can watch the ci/cd pipeline work and update production with this lol.'),
 ])
