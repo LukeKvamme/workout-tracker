a95df3732b1f97b2fa525b998a49516724c3dce4
Kvamme, Luke Brynjulv <lkvamme3@gatech.edu>
2025-11-20 10:13:18 -0500

    set_number handled automatically, displays prev sets during workout, grafana dashboard added to /analytics page, fixed grafana board not displaying when on Tailscale

--- pages/analytics.py
+++ pages/analytics.py
@@ -1,17 +1,31 @@
 from dash import callback_context, html, dcc, callback, Input, Output
+from flask import request
 import dash
 
 dash.register_page(__name__)
 
-grafana_dashboard_url = "http://192.168.1.111:3000/public-dashboards/8e29c895422d4bc1a0eff1accb92aec7"
+local_grafana_dashboard_url = "http://192.168.1.111:3000/public-dashboards/8e29c895422d4bc1a0eff1accb92aec7"
+tailscale_grafana_dashboard_url = "http://100.100.143.61:3000/public-dashboards/8e29c895422d4bc1a0eff1accb92aec7"
 
 layout = html.Div(children=[
     html.H1('Workout Stats'),
     html.Iframe(
-        src=grafana_dashboard_url,
+        id="grafana-output",
+        src=tailscale_grafana_dashboard_url, # tailscale default value bcz >90% access from gym
         width="100%",
         height="1450",
         style={"border": "5px solid black"}
-        )
+        ),
+    html.Div(id="dummy-hidden-div", style={"display": "none"})
     ]
 )
+
+@callback(
+    Output('grafana-output', 'src'),
+    Input('dummy-hidden-div', 'children')
+)
+def get_connection_url(children):
+    if request.remote_addr.startswith("100."):
+        return tailscale_grafana_dashboard_url
+    else:
+        return local_grafana_dashboard_url
